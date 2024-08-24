8b800090f665f2e79c58279f7166b034f05360c7
hamzakamran <hamzakwebdev@gmail.com>
2024-08-24 00:51:32 -0500

    Update

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,1 @@
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string;
-export const BASE_URL = import.meta.env.VITE_BASE_URL as string;
-export const MANAGER_URL = import.meta.env.VITE_MANAGER_URL as string;
-export const PUBLIC_TOKEN = import.meta.env.VITE_PUBLIC_TOKEN as string;
